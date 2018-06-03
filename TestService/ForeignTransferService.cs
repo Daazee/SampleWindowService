b9a08533e224d33a0f8f0cf3761f0e98bf99122b
Adebayo Azeez <azeez.adedayo@yahoo.com>
2018-06-03 02:15:54 +0100

    Added sample window service project

---
+++ TestService/ForeignTransferService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TestService
+{
+    public partial class ForeignTransferService : ServiceBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly Timer timer = new Timer();
+
+        public ForeignTransferService()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            InitializeComponent();
+
+
+            timer.Enabled = false;
+            timer.AutoReset = false;
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+        }
+
+        public ForeignTransferService(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+
+
+        }
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Transfer();
+        }
+
+        void Transfer()
+        {
+            try
+            {
+
+                log.Info($"Transfer at {DateTime.Now.ToString()}");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally{
+                timer.Start();
+            }
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("Service started");
+            int interval = 60000; //every minute
+
+            if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["POLLING_INTERVAL_SECONDS"]))
+            {
+                try
+                {
+                    interval = Convert.ToInt32(ConfigurationManager.AppSettings["POLLING_INTERVAL_SECONDS"]) * 1000;
+                    timer.Interval = interval;
+                }
+                catch { }
+            }
+
+            log.Debug($"Checking for outbound transactions every {interval} ms");
+            Transfer();
+        }
+        protected override void OnStop()
+        {
+            log.Info("stopped");
+            timer.Stop();
+
+        }
+
+        protected override void OnContinue()
+        {
+            log.Info("continuing - checking for new transactions");
+
+            Transfer();
+
+        }
+
+        protected override void OnPause()
+        {
+            log.Info("pausing");
+            timer.Stop();
+        }
+    }
+}
---
+++ TestService/Installer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace TestService
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+
+            ServiceProcessInstaller serviceProcessInstaller =
+                               new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //# Service Account Information
+
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //# Service Information
+
+            serviceInstaller.DisplayName = "Sterling - CIB Foreign Transfer";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            //# This must be identical to the WindowsService.ServiceBase name
+
+            //# set in the constructor of WindowsService.cs
+
+            serviceInstaller.ServiceName = "ForeignTransferService";
+
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+
+        public Installer(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
